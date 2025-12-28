5b35ec8dcb6befc6cb5a19a3abdc6e765309b5aa
Dylan Cronk <dylancronk.dev@gmail.com>
2025-12-27 19:25:40 -0500

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Dylan Cronk on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
